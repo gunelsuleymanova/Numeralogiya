7f88cfef8bfabe46b629ddbc48636648afb5de57
gunelsuleymanova <gunels4242@gmail.com>
2026-06-19 11:08:22 +0400

    1

---
+++ Numeralogiya/src/main/java/com/example/Numeralogiya/NumeralogiyaApplication.java
@@ -0,0 +1,13 @@
+package com.example.Numeralogiya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NumeralogiyaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NumeralogiyaApplication.class, args);
+	}
+
+}
---
+++ Numeralogiya/src/test/java/com/example/Numeralogiya/NumeralogiyaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Numeralogiya;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NumeralogiyaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
